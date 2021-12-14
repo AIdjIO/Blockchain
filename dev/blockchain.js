9d0fc6e1e8ba2dab6e880eaec3258e4f3895995c
AidjIO <ai.dj@pm.me>
2021-12-14 09:51:15 +0000

    added sha256 hash function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require('sha256');
+
 function Blockchain () {
     this.chain = [];
     this.pendingTransactions = []; 
+
+    this.createNewBlock(100,'0','0');
 }
 
 // class BlockChain {
@@ -16,13 +20,13 @@ Blockchain.prototype.createNewBlock  = function (nonce, previousBlockHash, hash)
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.pendingTranscations,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
@@ -42,6 +46,25 @@ Blockchain.prototype.createNewTrasnsaction = function (amount, sender, receipien
     this.pendingTransactions.push(newTransaction);
 
     return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    while (hash.substring(0,4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        console.log(hash);
+    }
+
+    return nonce;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,43 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2134,'WERTG$TGER','TUIYOKJFG1234');
+// bitcoin.createNewBlock(2134,'WERTG$TGER','TUIYOKJFG1234');
 
-bitcoin.createNewTrasnsaction(100, 'ALEX123dET4RTETE','JEN245WERTWERT');
+// bitcoin.createNewTrasnsaction(100, 'ALEX123dET4RTETE','JEN245WERTWERT');
 
-console.log(bitcoin);
+// bitcoin.createNewBlock(122,'ykfgvds','fghkj');
+
+// bitcoin.createNewTrasnsaction(50, 'ALEX123dET4RTETE','JEN245WERTWERT');
+// bitcoin.createNewTrasnsaction(300, 'ALEX123dET4RTETE','JEN245WERTWERT');
+// bitcoin.createNewTrasnsaction(2000, 'ALEX123dET4RTETE','JEN245WERTWERT');
+
+// bitcoin.createNewBlock(987564061,'YUIOKGFDGS3245E','VBNMDFH567GHKJ');
+
+const previousBlockHash = 'DFG34HGFH5768JTGHJ';
+const currentBlockData = [
+    { amount:10,
+    sender: 'SDFG2345SDFG2345WR',
+    recipient:'WERT3245DHJYY5467'
+    },
+    { amount:30,
+        sender: 'SDFG2SDFG45SDFG2345WR',
+        recipient:'WERT32£456DHJYY5467'
+    },
+    { amount:200,
+      sender: 'SDFGDFG345SDFG2345WR',
+      recipient:'WERDFG34563eEFTYY5467'
+    }
+];
+
+// const nonce = 100;
+
+// newHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+
+newHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, 22182);
+
+console.log(newHash)
+// console.log(bitcoin);
+
+
